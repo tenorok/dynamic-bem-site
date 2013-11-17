220b182cafe3c6d9b18b2b9351f3cdfe378b796f
tenorok <ttenor@yandex.ru>
2013-11-17 23:24:32 +0400

    Настройка сборки проекта

--- .bem/levels/blocks.js
+++ .bem/levels/blocks.js
@@ -2,8 +2,6 @@ var PATH = require('path'),
     environ = require('bem-environ'),
     join = PATH.join,
 
-    PRJ_ROOT = environ.PRJ_ROOT,
-    PRJ_TECHS = join(PRJ_ROOT, '.bem/techs'),
     BEMCORE_TECHS = environ.getLibPath('bem-core', '.bem/techs');
 
 exports.getTechs = function() {
@@ -13,15 +11,9 @@ exports.getTechs = function() {
         'browser.js'    : join(BEMCORE_TECHS, 'browser.js.js'),
         'node.js'       : join(BEMCORE_TECHS, 'node.js.js'),
         'css'           : 'css',
-        'ie.css'        : 'ie.css',
-        'ie6.css'       : 'ie6.css',
-        'ie7.css'       : 'ie7.css',
-        'ie8.css'       : 'ie8.css',
-        'ie9.css'       : 'ie9.css',
-
         'bemhtml'       : join(BEMCORE_TECHS, 'bemhtml.js')
     };
 
 };
 
-exports.defaultTechs = ['css', 'browser.js', 'bemhtml'];
+exports.defaultTechs = ['css', 'js', 'bemhtml'];
